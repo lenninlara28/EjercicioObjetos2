6258c719871a3e6eeb58e26971089a6dfa21bb90
lenninlara28 <lenninlara28@hotmail.com>
2016-09-12 13:25:34 -0500

    clase y funcionalidad Suma creada

--- src/clases/NumeroMixto.java
+++ src/clases/NumeroMixto.java
@@ -10,5 +10,48 @@ package clases;
  * @author hp 14
  */
 public class NumeroMixto {
+    private int Parte_Entera;
+    private int Numerador;
+    private int Denominador;
+    
+    public NumeroMixto(int parte_entera,int numerador,int denominador){
+        this.Parte_Entera=parte_entera;
+        this.Numerador=numerador;
+        this.Denominador=denominador;
+    }
+
+    public int getParte_Entera() {
+        return Parte_Entera;
+    }
+
+    public void setParte_Entera(int Parte_Entera) {
+        this.Parte_Entera = Parte_Entera;
+    }
+
+    public int getNumerador() {
+        return Numerador;
+    }
+
+    public void setNumerador(int Numerador) {
+        this.Numerador = Numerador;
+    }
+
+    public int getDenominador() {
+        return Denominador;
+    }
+
+    public void setDenominador(int Denominador) {
+        this.Denominador = Denominador;
+    }
+    
+    public NumeroMixto Sumar (NumeroMixto mx2){
+        NumeroMixto mx;
+        int parte_entera,num,dem;
+        parte_entera= this.Parte_Entera + mx2.Parte_Entera;
+        num= this.Numerador * mx2.Denominador + this.Denominador * mx2.Numerador;
+        dem= this.Denominador * mx2.Denominador;
+        mx=new  NumeroMixto(parte_entera, num, dem);
+        return mx;
+    } 
     
 }
